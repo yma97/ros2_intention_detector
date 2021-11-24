09bf2df531f990c2bfbbd88b6a912ef84ed4c0f5
reachy <reachygit@protonmail.com>
2021-11-24 18:42:47 +0100

    add led indicator

--- ros2_intention_detector/intention_detector_function.py
+++ ros2_intention_detector/intention_detector_function.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-from reachyAudio import reachyAudio
+from pixel_ring import pixel_ring
 
 import speech_recognition as sr
 import time
@@ -10,6 +10,7 @@ import time
 class IntentionPublisher(Node):
 
     def __init__(self):
+
         """Initialization"""
         self.start_time = time.time()
         print("At {0:.2f}: Now we start!".format(time.time()-self.start_time))
@@ -17,9 +18,10 @@ class IntentionPublisher(Node):
         print("At {0:.2f}: Initializing...".format(time.time()-self.start_time))
         self.recognizer = sr.Recognizer()
         self.microphone = sr.Microphone()
-        self.reachy_audio = reachyAudio.ReachyAudio()
-        #self.reachy_audio.pixel_ring.mono(self.reachy_audio.COLORS['RED'])
-        self.reachy_audio.pixel_ring.mono(0xFF0000)
+
+
+        #self.reachy_audio = reachyAudio.ReachyAudio()
+        #self.reachy_audio.pixel_ring.mono(self.reachy_audio.COLORS['CYAN'])
         print("At {0:.2f}: Initialization done!".format(time.time()-self.start_time))
 
         # keep doing speach recognition
@@ -86,8 +88,10 @@ class IntentionPublisher(Node):
             recognizer.adjust_for_ambient_noise(source)
             print("At {0:.2f}: Start listening".format(time.time()-self.start_time))
             time_before_listen = time.time()
+            pixel_ring.mono(0x00FF00)
             audio = recognizer.listen(source, phrase_time_limit=8)
             time_listened = time.time()-time_before_listen
+            pixel_ring.mono(0xFF0000)
             print("At {0:.2f}: Finish listening".format(time.time()-self.start_time))
             if (time_listened >= 10):print("TIME OUT - listened more than 10 seconds")
 
